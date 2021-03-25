680c94e6bea66c6dafec4ea6cd8ad8d21ccb0ef1
Daniel Fernandez <daniel.ernesto.fernandez.flores@gmail.com>
2021-03-25 13:25:18 -0500

    registry-config

---
+++ registry-service/src/test/java/com/aureand/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aureand.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
